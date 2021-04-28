9cdfb2d52c2f83f57f12dd20cc3b2610e0fead9d
Moises Rivera <mhrivera324@gmail.com>
2021-04-27 21:08:34 -0600

    agregado archivo funciones

--- python_basico/funciones.py
+++ python_basico/funciones.py
@@ -1,10 +1,10 @@
-def imprimir_mensaje():
-    print("Mensaje especial: ")
-    print("Estoy aprendiendo a usar funciones ")
+# def imprimir_mensaje():
+#     print("Mensaje especial: ")
+#     print("Estoy aprendiendo a usar funciones ")
 
-imprimir_mensaje()
-imprimir_mensaje()
-imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
 
-
+opcion = input('Elige una opcion (1, 2, 3): ')
 
